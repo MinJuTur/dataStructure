5afda4b545d64df75a07646b28ff788b7f1d434a
MinJuTur <jmj4701jmj@gmail.com>
2025-03-31 09:40:26 +0900

    03.31 스택구현

---
+++ practice/Day0331/ArrayStack.py
@@ -0,0 +1,54 @@
+class ArrayStack:
+    def __init__(self, capacity = 20):
+        self.capacity = capacity
+        self.top = -1 # 상단의 요소를 가리킴
+        self.arr = [None] * capacity # 아무것도 없음을 전체에 채워넣기
+
+    def isEmpty(self):
+        return self.top == -1
+
+    def isFull(self):
+        return self.top == self.capacity - 1
+
+    def push(self, e):
+        if not self.isFull():
+            self.top += 1
+            self.arr[self.top] = e
+        else:
+            print('Overflow.')
+
+    def pop(self):
+        if not self.isEmpty():
+            e = self.arr[self.top]
+            self.top -= 1
+            return e
+        else:
+            print('Underflow.')
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.arr[self.top]
+        else:
+            print('Underflow.')
+
+    def display(self):
+        print()
+        for i in range(self.top, -1, -1):
+            print(' | %d |' % (self.arr[i]))
+            print(' -----')
+        print()
+
+if __name__ == '__main__':
+    S = ArrayStack()
+
+    data = [5,3,8,1,2,7]
+
+    for e in data:
+        S.push(e)
+    S.display()
+
+    print('Popped [%d]' % S.pop())
+    S.display()
+
+    print('Peeked [%d]' % S.peek())
+    S.display()
