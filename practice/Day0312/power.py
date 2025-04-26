b96653d8d893cbebb8a5aea37fbe0964b7b25a89
MinJuTur <jmj4701jmj@gmail.com>
2025-04-26 15:12:23 +0900

    제곱하는 함수에 단순 재귀 함수 추가

--- practice/Day0312/power.py
+++ practice/Day0312/power.py
@@ -6,7 +6,7 @@ def iPower(x,n):
 
     return result
 
-def rPower(x, n):
+def rPower(x, n): # 분할 정복 (n을 절반씩 나눔)
     if n == 0:
         return 1
     elif (n % 2) == 0:
@@ -14,5 +14,13 @@ def rPower(x, n):
     else:
         return x * rPower(x*x, (n-1)//2)
 
+def Power(x, n): # 단순 재귀
+    if n==0:
+        return 1
+    else:
+        return x*Power(x,n-1)
+
+
 print('iPower : %d' % (iPower(2, 10)))
 print('rPower : %d' % (rPower(2, 10)))
+print('Power : %d' % (Power(2, 10)))
