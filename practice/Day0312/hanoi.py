0b3f17cc99ca67d6dd4c8134137a389468aa4c46
MinJuTur <jmj4701jmj@gmail.com>
2025-04-26 14:03:41 +0900

    하노이탑 코드 주석 추가

--- practice/Day0312/hanoi.py
+++ practice/Day0312/hanoi.py
@@ -8,7 +8,7 @@ def hanoi(n, fr, tmp, to):  # 원반번호, 시작 기둥, 보조 기둥, 목표
         # Step 2: 가장 큰 원반을 목표 기둥 C로 이동
         print("Disk %d : %s -- > %s" % (n, fr, to)) # A -> C
 
-        # Step 3: B에 있는 n-1개를 C로 이동 (A를 보조로 사용)
+        # Step 3: 보조 기둥 B에 있는 n-1개를 목표 기둥 C로 이동 (A를 보조로 사용)
         hanoi(n - 1, tmp, fr, to) # B -> C
 
 hanoi(3, 'A', 'B', 'C')
