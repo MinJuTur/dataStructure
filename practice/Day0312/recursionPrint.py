1e0ff897ebddb645ca4bdecb426a62648f034144
MinJuTur <jmj4701jmj@gmail.com>
2025-04-26 15:31:04 +0900

    각 자리수 출력하는 함수 수정

--- practice/Day0312/recursionPrint.py
+++ practice/Day0312/recursionPrint.py
@@ -2,8 +2,8 @@ def rPrint(num):
     if num < 10:
         print(num)
     else:
-        rPrint(num // 10)
         print(num%10)
+        rPrint(num // 10)
         
 n = int(input("양의 정수를 입력: " ))
 rPrint(n)
